7f5072c1ee96d8510697c1f44d6f725ccda6e458
Bussy31 <philipp97bu@outlook.de>
2026-04-03 22:19:13 +0200

    Update

--- marktspiel.py
+++ marktspiel.py
@@ -364,7 +364,7 @@ elif st.session_state.ansicht == 'lehrer_dashboard':
 
     # --- TEST-TOOLS ---
     with st.expander("🧪 Test-Werkzeuge (nur für Entwicklung)", expanded=False):
-        if st.button("🤖 20 Dummy-Schüler & Gebote erzeugen"):
+        if st.button("20 Dummy-Schüler:innen & Gebote erzeugen"):
             generiere_testdaten(st.session_state.spiel_id)
             st.success("Testdaten generiert! Bitte Seite aktualisieren.")
             st.rerun()
